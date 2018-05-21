40e5d07ae7cc3b98dbe5f8500a5659ddcf3526b1
goldlekx <aurelian.alexandrescuaa@gmail.com>
2018-05-21 21:11:11 +0300

    create backend project

---
+++ src/main/java/com/proiect/pbr/PbrApplication.java
@@ -0,0 +1,12 @@
+package com.proiect.pbr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PbrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PbrApplication.class, args);
+	}
+}
